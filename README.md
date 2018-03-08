770f8470c06af73e0373291e587d17de79b0d5a0
a-hilaly <hilalyamine@gmail.com>
2018-03-08 09:14:10 +0100

    Add dependencies

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # snake
 experimental snake game
 
+## dependencies
+
+- g++
+- [libsfml](https://github.com/SFML/SFML)
+
 ## build
 
 `chmod +x build.sh`
 
 `./build.sh`
+
+run with `./snake.a`
 
 
 ## todo
--- src/snake.cpp
+++ src/snake.cpp
@@ -1,16 +1,12 @@
-//
-
 #include <SFML/Graphics.hpp>
 #include <time.h>
 
-//
 using namespace sf;
 
-
 int LENGHT = 30, HEIGHT = 20;
-int size = 16;
-int w = size * LENGHT;
-int h = size * HEIGHT;
+int S = 16;
+int W = S * LENGHT;
+int H = S * HEIGHT;
 
 int dir, num = 4;
 
@@ -22,13 +18,11 @@ struct FoodPos {
     int x, y;
 } FP;
 
-void Tick()
- {
-    for (int i = num; i>0; --i)
-	 {
-         snake[i].x = snake[i-1].x;
-	     snake[i].y = snake[i-1].y;
-     }
+void Tick(){
+    for (int i = num; i>0; --i){
+        snake[i].x = snake[i-1].x;
+	    snake[i].y = snake[i-1].y;
+    }
 
     if (dir == 0) snake[0].y += 1;
     if (dir == 1) snake[0].x -= 1;
@@ -41,18 +35,20 @@ void Tick()
         FP.y = rand() % HEIGHT;
     }
 
-    if (snake[0].x > LENGHT) snake[0].x = 0;  if (snake[0].x < 0) snake[0].x = LENGHT;
-    if (snake[0].y > HEIGHT) snake[0].y = 0;  if (snake[0].y < 0) snake[0].y = HEIGHT;
+    if (snake[0].x > LENGHT) snake[0].x = 0;
+    if (snake[0].x < 0) snake[0].x = LENGHT;
+    if (snake[0].y > HEIGHT) snake[0].y = 0;
+    if (snake[0].y < 0) snake[0].y = HEIGHT;
 
     for (int i = 1; i < num; i++)
      if (snake[0].x == snake[i].x && snake[0].y == snake[i].y)  num = i;
- }
+}
 
 int main()
 {
 	srand(time(0));
 
-    RenderWindow window(VideoMode(w, h), "Snake Game!");
+    RenderWindow window(VideoMode(W, H), "Snake Game!");
 
 	Texture t1,t2,t3;
 
@@ -70,15 +66,14 @@ int main()
 	FP.x = 10;
     FP.y = 10;
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
 		float time = clock.getElapsedTime().asSeconds();
 		clock.restart();
         timer += time;
 
         Event e;
-        while (window.pollEvent(e))
-        {
+
+        while (window.pollEvent(e)) {
             if (e.type == Event::Closed)
                 window.close();
 		}
@@ -93,16 +88,15 @@ int main()
     window.clear();
 
     for (int i = 0; i < LENGHT; i++)
-	  for (int j = 0; j < HEIGHT; j++)
-		{ sprite1.setPosition(i * size, j * size);  window.draw(sprite1); }
+	    for (int j = 0; j < HEIGHT; j++)
+		    { sprite1.setPosition(i * S, j * S);  window.draw(sprite1); }
 
 	for (int i = 0; i < num; i++)
-	    { sprite2.setPosition(snake[i].x * size, snake[i].y * size);  window.draw(sprite2); }
+	    { sprite2.setPosition(snake[i].x * S, snake[i].y * S);  window.draw(sprite2); }
 
-	sprite3.setPosition(FP.x * size,  FP.y * size); window.draw(sprite3);
+	sprite3.setPosition(FP.x * S,  FP.y * S); window.draw(sprite3);
 
 	window.display();
 	}
-
     return 0;
 }
